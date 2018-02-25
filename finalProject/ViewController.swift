370f54a72ba4a40703a5dc354e042b84436b087b
AndrewJenson <awjenson@gmail.com>
2018-02-25 10:05:05 -0800

    Initial Commit

---
+++ finalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalProject
+//
+//  Created by Andrew Jenson on 2/25/18.
+//  Copyright © 2018 Andrew Jenson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
